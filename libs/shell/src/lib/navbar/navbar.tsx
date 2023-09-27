fa4f60615877c059067265d45ac30d9ed72a18c4
The Fro Code <cmukiri808@gmail.com>
2023-09-27 12:02:28 +0300

    feat: add navbar routing

--- libs/shell/src/lib/navbar/navbar.tsx
+++ libs/shell/src/lib/navbar/navbar.tsx
@@ -2,18 +2,20 @@ import { Container } from '@spotify-clone/ui';
 import { FaHome, FaSearch, FaPlus } from 'react-icons/fa';
 import { navbar, navbarItem } from './navbar.css';
 import { UserLibrary } from '@spotify-clone/user-library';
+import { Link } from '@tanstack/react-location';
 
 interface NavbarItemProps {
   label: string;
   icon?: any;
+  to: string
 }
 
-function NavbarItem({ label, icon }: NavbarItemProps) {
+function NavbarItem({ label, icon, to }: NavbarItemProps) {
   return (
-    <div className={`${navbarItem}`}>
+    <Link to={to} className={`${navbarItem}`}>
       {icon}
       <div>{label}</div>
-    </div>
+    </Link>
   );
 }
 
@@ -21,9 +23,9 @@ export function Navbar() {
   return (
     <div className={`${navbar}`}>
       <Container flexDirection="column" margin="0px" flexShrink={0}>
-        <NavbarItem label="Home" icon={<FaHome size={24} />} />
+        <NavbarItem label="Home" to='/home'icon={<FaHome size={24} />} />
 
-        <NavbarItem label="Search" icon={<FaSearch size={24} />} />
+        <NavbarItem label="Search" to='/search' icon={<FaSearch size={24} />} />
       </Container>
       <UserLibrary/>
     </div>
